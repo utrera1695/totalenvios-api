bfb8be41eca3155ae14b19a056e01f6e79d033f2
Victor <utrera1695@gmail.com>
2020-03-26 12:36:54 -0400

    asd

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -1,26 +1,31 @@
-'use strict'
+'use strict';
 
-import sequelize from '../../config/db'
+import sequelize from '../../config/db';
 const Sequelize = require('sequelize');
-const User =
-  sequelize.define('ps_user_app', {
+const User = sequelize.define(
+  'ps_user_app',
+  {
     id_user: {
       type: Sequelize.INTEGER(11),
       primaryKey: true,
       unique: 'compositeIndex',
       autoIncrement: true
     },
-    email: Sequelize.TEXT,
-    password: Sequelize.TEXT,
-    name: Sequelize.TEXT,
-    business: Sequelize.TEXT,
+    email: { type: Sequelize.TEXT, allowNull: false },
+    password: { type: Sequelize.TEXT, allowNull: false },
+    nombre: { type: Sequelize.TEXT, defaultValue: '' },
+    empresa: { type: Sequelize.TEXT, defaultValue: '' },
+    direccion: { type: Sequelize.TEXT, defaultValue: '' },
+    telefono: { type: Sequelize.TEXT, defaultValue: '' },
 
     fecha_trans: {
       type: Sequelize.INTEGER(11)
     }
-  }, {
+  },
+  {
     timestamps: false,
     freezeTableName: true
-  })
-User.removeAttribute('id')
-export default User
+  }
+);
+User.removeAttribute('id');
+export default User;
