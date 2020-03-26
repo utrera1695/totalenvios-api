dbfddc072b17bb6217a7bc3dca2b0de3b4316734
Victor <utrera1695@gmail.com>
2020-03-26 12:46:28 -0400

    test

--- src/api/user/user.service.js
+++ src/api/user/user.service.js
@@ -1,58 +1,59 @@
-'use strict'
-import User from './user.model'
+'use strict';
+import User from './user.model';
 import bcrypt from 'bcryptjs';
 import jwtCreate from '../../config/jwt';
 
-const Login = async (body) => {
+const Login = async body => {
   let UserExist = User.findOne({
     where: {
       email: data.email
     }
-  })
+  });
   if (UserExist) {
     const compareHash = bcrypt.compareSync(body.password, UserExist.password);
     if (compareHash) {
       /* ENVIA EL TOKEN CON EL ROLE DEL USUARIO */
-      return ({
+      return {
         token: jwtCreate.createToken(UserExist),
         usuario: UserExist
-      })
+      };
     } else {
       throw {
         code: 400,
         error: 'Contraseña incorrecta usuario'
-      }
+      };
     }
   } else {
     throw {
       code: 400,
       error: 'No existe el usuario'
-    }
+    };
   }
-}
+};
 
-const Register = async (data) => {
+const Register = async data => {
   let UserExist = User.findOne({
     where: {
       email: data.email
     }
-  })
+  });
+  console.log(UserExist);
   if (!UserExist) {
     data.password = bcrypt.hashSync(data.password, 10);
     return await User.sync({
       force: false
-    }).then((e) => {
-      return User.create(data)
-    })
+    }).then(e => {
+      return User.create(data);
+    });
   } else {
     throw {
       code: 400,
       error: 'Ya existe un usuario con este email'
-    }
+    };
   }
-}
+};
 
 export default {
   Login,
   Register
-}
+};
