cb09a2f58ca1872cce30bf4d960896ceef1794ea
Victor <utrera1695@gmail.com>
2020-03-26 12:41:14 -0400

    correcion

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,28 +1,29 @@
-import express from "express";
-import UserService from "./user.service";
+import express from 'express';
+import UserService from './user.service';
 var router = express.Router();
 
-router.post("/login",
+router.post(
+  '/login',
   /* Guardar registro de pais */
   async (req, res) => {
     try {
-      let user = await UserService.Login(req.body)
-      res.status(200).send(user)
+      let user = await UserService.Login(req.body);
+      res.status(200).send(user);
     } catch (error) {
-      console.log(error)
-      res.status(error.code || 500).send(error)
+      console.log(error);
+      res.status(error.code || 500).send(error);
     }
-  });
+  }
+);
 
-router.post("/register",
-  async (req, res) => {
-    try {
-      let user = await UserService.FindSeguro(req.body)
-      res.status(200).send(user)
-    } catch (error) {
-      console.log(error)
-      res.status(error.code || 500).send(error)
-    }
-  })
+router.post('/register', async (req, res) => {
+  try {
+    let user = await UserService.Register(req.body);
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code || 500).send(error);
+  }
+});
 
 export default router;
