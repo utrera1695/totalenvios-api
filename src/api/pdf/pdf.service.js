8ab5034850cb7f8444cc8ecc3483541de2a7aa76
Victor Utrera <utrera1695@gmail.com>
2022-04-14 17:45:14 -0400

    fix toFixed error

--- src/api/pdf/pdf.service.js
+++ src/api/pdf/pdf.service.js
@@ -80,7 +80,7 @@ const createPdf = async data => {
                   ? ', con un peso volumetrico de ' +
                     (data.tiposistema
                       ? (data.peso /2.205).toFixed(2) + 'kg'
-                      : Number(data.peso).toFixed(2) + 'in')
+                      : data.peso.toFixed(2) + 'in')
                   : ', de ' + data.piescubicos + 'pies cúbicos'
               }
             </p>
